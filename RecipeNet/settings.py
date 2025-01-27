b02f4bb6cf3005b4254034af2eebdc5f92676e9b
TERUZvxght <terunori_kato@icloud.com>
2025-01-27 14:33:33 +0900

    vercel15

--- RecipeNet/settings.py
+++ RecipeNet/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "z-i19raw89&qt9&hs1gas+8pm)$&a!x9#s&twe*-bmm5b_+=7^"
 DEBUG = False
 
-ALLOWED_HOSTS = ['recipe-net.vercel.app', '127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['*', 'recipe-net.vercel.app',
+                 '127.0.0.1', '0.0.0.0', 'localhost']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
